353bdab00da51d41ad8ced8f34d6a8aac0cd5d0a
LuccaHaddadSerejo <hslucca@gmail.com>
2024-03-14 07:34:35 -0300

    feature (footer): Created footer component and styles

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import { StyledFooter } from "./style";
+
+const Footer = () => {
+  return (
+    <StyledFooter>
+      <p>© 2022 Coral, Inc.</p>
+    </StyledFooter>
+  );
+};
+
+export default Footer;
