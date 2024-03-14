db60faa49cb9e418c40d863ac38a87350d4883a2
LuccaHaddadSerejo <hslucca@gmail.com>
2024-03-14 08:04:58 -0300

    feature (pagination): Createad pagination component and styles

---
+++ src/components/Pagination/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyledPagination = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+
+  span {
+    font-weight: var(--font-weight-normal);
+    font-size: var(--font-size-450);
+    font-family: var(--font-style-opensans);
+  }
+`;
