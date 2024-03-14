8ee276da2fc4dcd7e8a43fed682339203339f9c6
LuccaHaddadSerejo <hslucca@gmail.com>
2024-03-14 07:51:32 -0300

    feature (product detail): Created product detail component page and styles

---
+++ src/pages/ProductDetail/style.ts
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const StyledProductDetailMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const StyledContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 95%;
+  gap: 64px;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+
+  & > :nth-child(2) {
+    & > h3 {
+      margin-bottom: 9px;
+      color: var(--color-dark-base);
+      font-size: var(--font-size-850);
+      font-weight: var(--font-weight-medium);
+      font-family: var(--font-style-roboto);
+    }
+
+    & > span {
+      margin-bottom: 9px;
+      color: var(--color-dark-base);
+      font-size: var(--font-size-600);
+      font-weight: var(--font-weight-normal);
+      font-family: var(--font-style-roboto);
+    }
+
+    & > p {
+      margin-top: 24px;
+      margin-bottom: 24px;
+      color: var(--color-dark-base);
+      font-size: var(--font-size-450);
+      font-weight: var(--font-weight-normal);
+      font-family: var(--font-style-opensans);
+    }
+  }
+`;
